74ecd448d33721f06c78e9347a3e5df1bd0c9ae6
gjssss <gaojinsong2010@gmail.com>
2024-02-20 02:18:30 +0800

    fix: color theme and change icon

--- components/Navigation.vue
+++ components/Navigation.vue
@@ -19,10 +19,18 @@ const colorMode = useColorMode()
         <div class="i-carbon-logo-github cursor-pointer text-5" />
       </nuxt-link>
       <div v-bind="slideToggleDark" class="relative h-6 w-6 of-hidden rounded-8">
-        <div class="theme-icon-group absolute left-0 top-0 h-8 w-8" :class="{ dark: colorMode.preference === 'dark' }">
-          <div class="i-carbon-sun ml-4 cursor-pointer text-5" />
-          <div class="i-carbon-moon mt--2 cursor-pointer text-5" />
-        </div>
+        <ClientOnly>
+          <div class="theme-icon-group absolute left-0 top-0 h-8 w-8" :class="{ dark: colorMode.value === 'dark' }">
+            <div class="i-carbon-sun ml-4 cursor-pointer text-5" />
+            <div class="i-carbon-moon mt--2 cursor-pointer text-5" />
+          </div>
+          <template #fallback>
+            <div class="theme-icon-group absolute left-0 top-0 h-8 w-8">
+              <div class="i-carbon-sun ml-4 cursor-pointer text-5" />
+              <div class="i-carbon-moon mt--2 cursor-pointer text-5" />
+            </div>
+          </template>
+        </ClientOnly>
       </div>
     </div>
   </div>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -25,7 +25,6 @@ export default defineNuxtConfig({
 
   colorMode: {
     classSuffix: '',
-    preference: 'dark',
   },
 
   nitro: {
